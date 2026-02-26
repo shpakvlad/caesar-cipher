e9cda8c5b40bfa259ac58c2545ef10399b1a030a
shpakvlad <vladyslav.shpakov@gmail.com>
2026-02-26 19:31:51 +0200

    Removed not used variables

--- cc.rb
+++ cc.rb
@@ -8,11 +8,9 @@ user_shift = gets
 def caesar_ruby_style(text, shift)
   source = ('a'..'z').to_a.join + ('A'..'Z').to_a.join
 
-  shifted_lower = ('a'..'z').to_a.rotate(shift).join + ('A'..'Z').to_a.rotate(shift).join
+  shifted_text = ('a'..'z').to_a.rotate(shift).join + ('A'..'Z').to_a.rotate(shift).join
 
-  destination = shifted_lower
-
-  text.tr(source, destination)
+  text.tr(source, shifted_text)
 end
 
 puts caesar_ruby_style(user_text, user_shift.to_i)
